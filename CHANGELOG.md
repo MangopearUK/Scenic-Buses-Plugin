8e21e8ae1122b26cef463e3e37d2923bd83c73e5
Mangopear creative <andi@mangopear.co.uk>
2018-09-02 01:42:16 +0100

    Bump versions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 3.1.0
+
+* Added collections taxonomy to routes post type
+
 ## 3.0.0
 
 * Initial release version
--- post-types/mangopear.post-type.routes.php
+++ post-types/mangopear.post-type.routes.php
@@ -8,9 +8,9 @@
 	 * @author  	Andi North <andi@mangopear.co.uk>
 	 * @copyright  	2017 Mangopear creative
 	 * @license   	GNU General Public License <http://opensource.org/licenses/gpl-license.php>
-	 * @version  	3.0.0
+	 * @version  	3.1.0
 	 * @link 		https://mangopear.co.uk/
-	 * @since   	3.0.0
+	 * @since   	3.1.0
 	 */
 	
 
