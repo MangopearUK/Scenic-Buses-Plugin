06424bfb4248938288de13b882481b05191a20ca
Andi North <andi@mangopear.co.uk>
2024-03-13 21:58:08 +0000

    Add oAuth2 plugin

---
+++ helpers/oauth2/index.php
@@ -0,0 +1,17 @@
+<?php
+
+	/**
+	 * This is a hidden page for security reasons.
+	 *
+	 * Do not delete this file as it could lead to vulnerabilities in the code base.
+	 *
+	 * @category 	security-placeholder
+	 * @package  	pearfect
+	 * @author  	Andi North <andi@theverycreativefruitbowl.co.uk>
+	 * @copyright  	2023 The Very Creative Fruit Bowl Company
+	 * @license   	Proprietary license - You can not use this code without permission
+	 * @version  	1.0.0
+	 * @since   	1.0.0
+	 */
+	
+?>
